a5dac504d5af13053c2fb19968555f2aa95597cd
Đinh Kiến Quốc <quocdkph28915@fpt.edu.vn>
2023-08-22 15:59:20 +0700

    Sửa thanh máu

---
+++ Assets/Script/EnemyHealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    public float HP = 200;
+    public float maxHP = 200;
+    public Image healthBar;
+    public void ReduceHP(float damege)
+    {
+        HP -= damege;
+        Health(HP, maxHP);
+
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Health(float hp, float maxHp)
+    {
+        healthBar.fillAmount=hp/maxHp;
+    }
+}
